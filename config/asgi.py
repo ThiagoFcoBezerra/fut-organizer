56c9ee012e77b3374adaaa992e138df7aa2cd062
Thiago Bezerra <thiagofcobezerra@hotmail.com>
2025-12-21 18:44:08 -0300

    nova Correção do asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,18 +1,23 @@
+# config/asgi.py
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+import django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
+# imports do seu projeto (podem puxar models) só depois do django.setup()
 from core.middleware import JwtAuthMiddleware
 from core.routing import websocket_urlpatterns
 
-
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": JwtAuthMiddleware(URLRouter(websocket_urlpatterns)),
-})
-
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": JwtAuthMiddleware(URLRouter(websocket_urlpatterns)),
+    }
+)
