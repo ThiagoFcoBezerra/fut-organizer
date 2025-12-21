1e1182feb3bc12ecd3d1cc984791d406e1de7ae5
Thiago Bezerra <thiagofcobezerra@hotmail.com>
2025-12-21 18:39:02 -0300

    Correção do asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,13 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from core.middleware import JwtAuthMiddleware
 from core.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
